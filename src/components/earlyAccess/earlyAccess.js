bf9fa1ad96d2d5eb798d5197c58d6b8ea17f260e
Kiryu <13matheusilva@gmail.com>
2024-04-08 18:57:07 -0300

    early access section style add

--- src/components/earlyAccess/earlyAccess.js
+++ src/components/earlyAccess/earlyAccess.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+import './earlyAccess.css'
+
 const EarlyAccess = () => {
     return (
         <section className="early-access">
-            <div className="info">
-                <h2 className="title">Get early access today</h2>
-                <p className="description">It only takes a minute to sign up and our free starter tier is extremely generous.
-                    If you have any questions, our support team would be happy to help you.</p>
-            </div>
-            <div className="inputs">
-                <input type="email" placeholder="email@example.com"></input>
-                <input type="submit" className="submitInput" value='Get Started For Free'></input>
+            <div className="early-access-content">
+                <div className="info">
+                    <h2 className="title">Get early access today</h2>
+                    <p className="description">It only takes a minute to sign up and our free starter tier is extremely generous.
+                        If you have any questions, our support team would be happy to help you.</p>
+                </div>
+                <div className="inputs">
+                    <input type="email" placeholder="email@example.com" className="emailInput"></input>
+                    <input type="submit" className="submitInput" value='Get Started For Free'></input>
+                </div>
             </div>
         </section>
     )
