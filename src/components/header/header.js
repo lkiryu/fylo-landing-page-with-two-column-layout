d1b9ad33464e4a55fb1626b19b92ecf7932f0880
Kiryu <13matheusilva@gmail.com>
2024-04-08 15:51:59 -0300

    header component add

---
+++ src/components/header/header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from '../../images/logo.svg'
+
+const Header = () =>{
+    return(
+        <header className="header">
+            <a href="/">
+                <img src={logo} alt="logo" className="logo"></img>
+            </a>
+
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-link"><a href="/">Features</a></li>
+                    <li className="nav-link"><a href="/">Team</a></li>
+                    <li className="nav-link"><a href="/">Sign In</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../header/header";
 
 const LandingPage = () =>{
     return(
         <main className="container">
-            
+            <Header />
         </main>
     )
 }
