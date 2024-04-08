fc2531a32be35de55946f433608eca504988e8ad
Kiryu <13matheusilva@gmail.com>
2024-04-08 17:52:43 -0300

    hero section style add

--- src/components/sectionHero/sectionHero.js
+++ src/components/sectionHero/sectionHero.js
@@ -1,22 +1,25 @@
 import React from "react";
-
 import illustration from '../../images/illustration-1.svg'
+
+import './sectionHero.css'
 
 const Hero = () => {
     return (
-        <section className="Hero">
-            <div className="info">
-                <h2 className="title">All your files in one secure location, accessible anywhere.</h2>
-                <p className="description">Fylo stores your most important files in one secure location.
-                    Access them wherever you need, share and collaborate with friends,
-                    family, and co-workers.</p>
-                <div className="inputs">
-                    <input type="email" placeholder="Enter your email..."></input>
-                    <input type="submit" className="submitInput" value='Get Started'></input>
+        <section className="hero">
+            <div className="hero-content">
+                <div className="info">
+                    <h2 className="title">All your files in one secure location, accessible anywhere.</h2>
+                    <p className="description">Fylo stores your most important files in one secure location.
+                        Access them wherever you need, share and collaborate with friends,
+                        family, and co-workers.</p>
+                    <div className="inputs">
+                        <input type="email" className="emailInput" placeholder="Enter your email..."></input>
+                        <input type="submit" className="submitInput" value='Get Started'></input>
+                    </div>
                 </div>
-            </div>
 
-            <img src={illustration} alt="hero illustation"></img>
+                <img src={illustration} alt="hero illustation"></img>
+            </div>
         </section>
     )
 }
